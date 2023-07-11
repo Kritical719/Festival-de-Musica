ca0e00f89c35cf10df167abd6d8c502dd74bcba1
Kritical <javier.araya23@gmail.com>
2023-07-11 00:02:14 -0400

    Actualizado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,72 +1,59 @@
+import  { useEffect } from 'react';
+
 const Header = () => {
+  const navegacionFija = () => {
+    useEffect(() => {
+      const barra = document.querySelector('.header') as HTMLElement | null;
+      const sobreFestival = document.querySelector('.sobre-festival') as HTMLElement | null;
+      const body = document.querySelector('body') as HTMLElement | null;
 
+      const handleScroll = () => {
+        if (sobreFestival?.getBoundingClientRect().bottom! < 0) {
+          barra?.classList.add('fijo');
+          body?.classList.add('body-scroll');
+        } else {
+          barra?.classList.remove('fijo');
+          body?.classList.remove('body-scroll');
+        }
+      };
 
-  function navegacionFija(){
-    const barra = document.querySelector('.header');
-    const sobreFestival = document.querySelector('.sobre-festival');
-    const body = document.querySelector('body');
+      window.addEventListener('scroll', handleScroll);
 
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+  };
 
-    window.addEventListener('scroll', function(){
-     
-
-      if(sobreFestival?.getBoundingClientRect().bottom < 0){
-        barra?.classList.add('fijo');
-        body?.classList.add('body-scroll');
-      }else {
-        barra?.classList.remove('fijo');
-        body?.classList.remove('body-scroll');
-      }
-    })
-  }
-navegacionFija();
-
-
-
-
-
-
-
-
-  function scrollNav(){
-    
+  const scrollNav = () => {
     const enlaces = document.querySelectorAll('.navegacion-principal');
-
-      enlaces.forEach(enlace =>{
-      enlace.addEventListener('click', function(e){
+  
+    enlaces.forEach((enlace) => {
+      enlace.addEventListener('click', (e) => {
         e.preventDefault();
-        const seccionScroll = e.target.attributes.href.value;
-        const seccion = document.querySelector(seccionScroll);
-        seccion.scrollIntoView({behavior: "smooth"});
+        const seccionScroll = (e.target as HTMLAnchorElement).getAttribute('href');
+        const seccion = document.querySelector(seccionScroll!);
+        seccion?.scrollIntoView({ behavior: 'smooth' });
+      });
+    });
+  };
+  
 
-      })  
-    })
-  }
+  navegacionFija();
 
-
-
- 
   return (
-    <>
-    
-
     <header className="header">
-        <div className="contenedor contenido-header">
-            <h1>Rock & EDM Festival</h1>
+      <div className="contenedor contenido-header">
+        <h1>Rock & EDM Festival</h1>
 
-            <nav onScroll={navegacionFija} onClick={scrollNav} className="navegacion-principal">
-                <a href="#lineup" >Line Up</a>
-                <a href="#galeria" >Galería</a>
-                <a href="#boletos" >Boletos</a>
-            </nav>
-
-
-        </div>
+        <nav onClick={scrollNav} className="navegacion-principal">
+          <a href="#lineup">Line Up</a>
+          <a href="#galeria">Galería</a>
+          <a href="#boletos">Boletos</a>
+        </nav>
+      </div>
     </header>
-    
-
-    </>
-  )
-}
+  );
+};
 
 export default Header;
